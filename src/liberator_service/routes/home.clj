8728bc9fffd975fcf273bcced727748e8481fd99
mjamesruggiero <mjamesruggiero@gmail.com>
2014-09-07 22:18:43 -0700

    first commit

---
+++ src/liberator_service/routes/home.clj
@@ -0,0 +1,12 @@
+(ns liberator-service.routes.home
+  (:require [compojure.core :refer :all]
+            [liberator.core
+             :refer [defresource resource request-method-in]]))
+
+;(defn home []
+  ;(layout/common [:h1 "Hello World!"]))
+
+(defroutes home-routes
+  (ANY "/" request (resource :handle-ok "Hello world!"
+                             :etag "fixed-etag"
+                             :available-media-types ["text/plain"])))
